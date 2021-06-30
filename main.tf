fa6daf6d3476b66732cef5fe6a42fe60e1b68832
ashutosh <yadav.ashu2496@gmail.com>
2021-06-30 13:10:45 +0530

    Terraform

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-00399ec92321828f5 "
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-test"
+  }
+}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.33.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+subnet_prefix = [{ cidr_block = "172.33.1.0/24", name = "prod_subnet" }, { cidr_block = "172.33.2.0/24", name = "dev_subnet" }]
